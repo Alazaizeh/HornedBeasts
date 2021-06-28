c686ed554bea5ac0522c3604f91fdbe402329b6a
Omar Alazaizeh <omx302@gmail.com>
2021-06-28 15:05:03 +0300

    lab02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import HornedBeasts from "./HornedBeasts";
 import beasts from "./data.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 class Main extends Component {
   render() {
     return (
-      <div>
+      <div className="grid">
         {beasts.map((beast, index) => (
           <HornedBeasts key={index} beast={beast} />
         ))}
