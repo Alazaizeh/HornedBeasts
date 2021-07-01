e8d6d0d96a58c7e31ac22e09f1c2b83781445be5
Omar Alazaizeh <omx302@gmail.com>
2021-07-01 11:01:25 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,6 +32,7 @@ class Main extends Component {
               <option value={true}>All</option>
               <option value={1}>One</option>
               <option value={2}>Two</option>
+              <option value={3}>Three</option>
               <option value={100}>WoW!</option>
             </Form.Control>
           </Form.Group>
